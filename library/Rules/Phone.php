e39e15a18135a66ffa0d132cf6568a3dcde8f5d0
Łukasz Socha <kontakt@lukasz-socha.pl>
2019-02-27 14:27:51 +0100

    Poprawa filtrowania nr telefonu
    Poprawa filtrowania nr telefonu

--- library/Rules/Phone.php
+++ library/Rules/Phone.php
@@ -5,6 +5,7 @@ class Phone extends AbstractRule
 {
     public function validate($input)
     {
+        $input=str_replace(array(' ', '-'), array('', ''), $input);
         return !empty($input) && preg_match('/^[+]?([\d]{0,3})?[\(\.\-\s]?(([\d]{1,3})[\)\.\-\s]*)?(([\d]{3,5})[\.\-\s]?([\d]{4})|([\d]{2}[\.\-\s]?){4})$/', $input);
     }
 }
